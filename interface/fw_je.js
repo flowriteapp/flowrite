30332ea5d40be85b9048659827bbf692d4bf35cb
rahulmahendru <32964204+rahulmahendru@users.noreply.github.com>
2019-11-03 22:27:40 -0800

    Interface design files

---
+++ interface/fw_je.js
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>FloWrite</title>
+        <link rel="stylesheet" href="css/fw_login.css" />
+    </head>
+    <body>
+        <section class="section">
+            <div class="container is-fluid">
+                <div class="columns">
+                    <div class="column is-1">
+                        <figure class="image is-96x96 is-inline-block">
+                            <a href="fw_home.html"><img src="images/flowrite.jpg" alt="FloWrite logo" /></a>
+                        </figure>
+                    </div>
+                    <div class="column is-3 is-offset-3 has-text-centered">
+                        
+                        <h1 class="title is-1">Journal Entry</h1>
+                        <p class="subtitle is-6 has-text-weight-semibold">Created Oct. 19 2019</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="container is-fluid">
+                <div class="field">
+                    <div class="control">
+                        <textarea class="textarea" placeholder="Your text here" rows="15"></textarea>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
